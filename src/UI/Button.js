25fad1fbd7ba5a1e84edce944f9cff95a350118f
Vadim <65212516+VadimChorrny@users.noreply.github.com>
2020-10-12 15:26:31 +0300

    create one button with storybook

---
+++ src/UI/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./index.css";
+
+const Button = () =>{
+    return(
+        <button>Тык</button>  
+    )
+}
+export default Button
---
+++ src/UI/Button.stories.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Button from "./Button";
+
+export default{
+    title: "ui/button",
+    component: Button
+}
+
+const Template = (args) => <Button {...args} ></Button>
+
+export const main = Template.bind({})
+
+main.args = {
+
+}
+
